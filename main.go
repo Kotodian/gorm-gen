710872993b812a19b43337fbfe30bbea8afbf1af
linqiankai <417350372@qq.com>
2021-05-28 09:16:18 +0800

    main完善

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
+var (
+	dir    = flag.String("dir", "", "source dir")
+	output = flag.String("output", "", "output dir")
+)
+
+func Usage() {
+	_, _ = fmt.Fprintf(os.Stderr, "Usage of gorm-gen:\n")
+	flag.PrintDefaults()
+}
+
+func main() {
+	flag.Usage = Usage
+	flag.Parse()
+	if len(*dir) == 0 {
+		flag.Usage()
+		os.Exit(2)
+	}
+	g := &Generator{output: *output}
+	g.generate(*dir)
 }
